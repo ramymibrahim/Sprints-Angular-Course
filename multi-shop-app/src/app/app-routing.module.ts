db579778883652d69725b3104aece1b1aaa8c2f3
Ramy Ibrahim <r.ibrahim@prosylab.com>
2022-09-29 16:47:32 +0200

    Session3

--- multi-shop-app/src/app/app-routing.module.ts
+++ multi-shop-app/src/app/app-routing.module.ts
@@ -5,15 +5,22 @@ import { CheckOutComponent } from './components/check-out/check-out.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { DetailComponent } from './components/detail/detail.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { ShopComponent } from './components/shop/shop.component';
+import { AuthGuardGuard } from './services/auth-guard.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'shop', component: ShopComponent },
   { path: 'cart', component: CartComponent },
-  { path: 'checkout', component: CheckOutComponent },
+  {
+    path: 'checkout',
+    component: CheckOutComponent,
+    canActivate: [AuthGuardGuard],
+  },
   { path: 'contact', component: ContactComponent },
   { path: 'detail/:id', component: DetailComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
